2556f82661a2ab798e1b74bd0587b2cf26e1033a
Sohrab Vahidli <esev.sv@gmail.com>
2021-09-18 20:06:45 +0400

    singlechildsiz ishleyir

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:caspa_mobility/copy/test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 import 'in_app_webiew_example.screen.dart';
@@ -15,7 +17,10 @@ Future main() async {
   // await Permission.camera.request();
   // await Permission.microphone.request();
   // await Permission.storage.request();
-
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.blue, // navigation bar color
+    statusBarColor: Colors.pink, // status bar color
+  ));
   if (Platform.isAndroid) {
     await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
 
@@ -34,72 +39,79 @@ Future main() async {
           return null;
         },
       );
+
+
     }
   }
 
-  runApp(MyApp());
+  //void main() => runApp(MaterialApp(home: WebViewExample()));
+
+  runApp(MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      home: InAppWebViewExampleScreen()));
 }
-
-Drawer myDrawer({@required BuildContext context}) {
-  return Drawer(
-    child: ListView(
-      padding: EdgeInsets.zero,
-      children: <Widget>[
-        DrawerHeader(
-          child: Text('flutter_inappbrowser example'),
-          decoration: BoxDecoration(
-            color: Colors.blue,
-          ),
-        ),
-        ListTile(
-          title: Text('InAppBrowser'),
-          onTap: () {
-            Navigator.pushReplacementNamed(context, '/InAppBrowser');
-          },
-        ),
-        ListTile(
-          title: Text('ChromeSafariBrowser'),
-          onTap: () {
-            Navigator.pushReplacementNamed(context, '/ChromeSafariBrowser');
-          },
-        ),
-        ListTile(
-          title: Text('InAppWebView'),
-          onTap: () {
-            Navigator.pushReplacementNamed(context, '/');
-          },
-        ),
-        ListTile(
-          title: Text('HeadlessInAppWebView'),
-          onTap: () {
-            Navigator.pushReplacementNamed(context, '/HeadlessInAppWebView');
-          },
-        ),
-      ],
-    ),
-  );
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(initialRoute: '/', routes: {
-      '/': (context) => InAppWebViewExampleScreen(),
-    });
-  }
-}
+//
+// Drawer myDrawer({@required BuildContext context}) {
+//   return Drawer(
+//     child: ListView(
+//       padding: EdgeInsets.zero,
+//       children: <Widget>[
+//         DrawerHeader(
+//           child: Text('flutter_inappbrowser example'),
+//           decoration: BoxDecoration(
+//             color: Colors.blue,
+//           ),
+//         ),
+//         ListTile(
+//           title: Text('InAppBrowser'),
+//           onTap: () {
+//             Navigator.pushReplacementNamed(context, '/InAppBrowser');
+//           },
+//         ),
+//         ListTile(
+//           title: Text('ChromeSafariBrowser'),
+//           onTap: () {
+//             Navigator.pushReplacementNamed(context, '/ChromeSafariBrowser');
+//           },
+//         ),
+//         ListTile(
+//           title: Text('InAppWebView'),
+//           onTap: () {
+//             Navigator.pushReplacementNamed(context, '/');
+//           },
+//         ),
+//         ListTile(
+//           title: Text('HeadlessInAppWebView'),
+//           onTap: () {
+//             Navigator.pushReplacementNamed(context, '/HeadlessInAppWebView');
+//           },
+//         ),
+//       ],
+//     ),
+//   );
+// }
+//
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => new _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+//
+//   @override
+//   void dispose() {
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(initialRoute: '/', routes: {
+//       '/': (context) => WebViewExample(),
+//     });
+//   }
+// }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:caspa_mobility/in_app_webiew_example.screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +14,7 @@ import 'package:caspa_mobility/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(InAppWebViewExampleScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
